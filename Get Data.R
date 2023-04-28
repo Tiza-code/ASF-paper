d48f18b02b6f92f4267f6c58f77d6b45bd80762a
Tiza-code <tiza.mfuni@gmail.com>
2023-04-28 09:06:46 -0400

    Update 27042023

--- Get Data.R
+++ Get Data.R
@@ -1,37 +1,37 @@
-#### Codes to organize and call file names
+#### Codes to organize and call file names for DHS and FAO data analysis
+### By Tiza Mfuni
+# Spring 2023
 
-###
 
-
-## First level
+## First level directory
 Lev_0 <- "C:/Users/Tiza Mfuni/Documents/Penn school/PhD Courses/PhD proposal and papers/0_ASF_consumption_and_export_paper"
 
 
-### Main data types by origin: Either DHS or FAO
+### Second level directory: Main data types by origin: Either DHS or FAO
 Lev_1DHS <- paste(Lev_0, "1_DHS_data", sep = "/")
 Lev_1FAOWD <- paste(Lev_0, "1_FAOWD", sep = "/")
 
 
 ###Subfolders in DHS level data
 Lev_2Demo <- paste(Lev_1DHS, "2_Demographic_data", sep = "/")
-Lev2Lev_2GIS <- paste(Lev_1DHS, "2_GIS_data", sep = "/")
+Lev_2GIS <- paste(Lev_1DHS, "2_GIS_data", sep = "/")
 
 
 ##Subfolders in DHS Demographic Data based on country of interest
 
-Lev_3ZM <- paste(Lev_2Demo, "3_ZM", sep = "/")
+Lev_3ZM <- paste(Lev_2Demo, "3_ZM_2018_DHS_01162023", sep = "/")
 
 # Lev_3TZ <- paste(Lev_2Demo, "1_TZ", sep = "/")
 # Lev_3MW <- paste(Lev_2Demo, "1_MW", sep = "/")
 # Lev_3KE <- paste(Lev_2Demo, "1_KE", sep = "/")
 
 ##Subfolders in Country Data for Women's file 
-Lev_4WO <- paste(Lev_3ZM, "4_WO", sep = "/")
+Lev_4WO <- paste(Lev_3ZM, "4_ZMIR71FL", sep = "/")
 
 
 ##Subfolders in DHS GIS Data based on country of interest
 
-Lev_3ZM <- paste(Lev_2GIS, "3_ZM", sep = "/")
+# Lev_3ZM <- paste(Lev_2GIS, "3_ZM", sep = "/")
 
 # Lev_3TZ <- paste(Lev_2GIS, "1_TZ", sep = "/")
 # Lev_3MW <- paste(Lev_2GIS, "1_MW", sep = "/")
@@ -42,20 +42,30 @@ Lev_3ZM <- paste(Lev_2GIS, "3_ZM", sep = "/")
 
 # Construct file name using paste()
 
-CountryCode <- "ZM"
-Type <- "WO"
-Year <- "18"
 
-GetDHS <- function(CountryCode,Type,Year){
+GetDHS <- function(CountryCode,Type,LastFour,foldername){
   
-  filename <- paste(CountryCode,Type,Year, ".dat", sep = "")
-  foldername <- paste(Lev_1DHS,CountryCode,Type,Year, sep = "/")
-  MyData <- read.csv(paste(foldername,filename,sep = "/"))
+  require(readr)
+  filename <- paste(CountryCode,Type,LastFour, ".dat", sep = "")
+  MyData <- read_table(paste(foldername,filename,sep = "/"), col_names = FALSE)
+  
   return(MyData)
   
 }
 
-MyData <- GetDHS("ZM","WO","18")
+
+
+CountryCode <- "ZM" 
+Type <- "IR"
+# Year <- "71FL"
+LastFour <- "71FL"
+foldername <- Lev_4WO
+
+
+out <- GetDHS(CountryCode,Type,LastFour,foldername)
+
+
 
 ##GetFAO function to get FAO data
-
+CountryCode,Type,Year,
+ZMIR71FL
